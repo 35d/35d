4dc3d958540c049c85d35d304a4008f3a6fbd935
35d <xxx35d@gmail.com>
2019-12-29 00:18:46 +0900

    ✨ google search console setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   title: '35d',
   description: '@___35d のブログサイトです',
-  ga: 'UA-148970528-1'
+  ga: 'UA-148970528-1',
+  head: [
+    ['google-site-verification', { content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' }]
+  ]
 }
