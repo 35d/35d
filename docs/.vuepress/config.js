f3ac60f01415998324dc2789ee137e5bfff984d0
35d <xxx35d@gmail.com>
2019-12-29 01:47:46 +0900

    ✨ add twitter card property

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: '35d',
   head: [
     ['meta', { name: 'google-site-verification', content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' }],
+    ['meta', { property: 'twitter:card', content: 'summary' }],
     ['link', { rel: 'icon', href: '/favicon.png' }],
   ],
   plugins: [
