a3b31118fb8289d98cc9ffa6d81e1005deead662
35d <xxx35d@gmail.com>
2019-12-29 00:30:23 +0900

    ✨ add GA plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,16 @@
 module.exports = {
   title: '35d',
   description: '@___35d のブログサイトです',
-  ga: 'UA-148970528-1',
   head: [
     ['meta', { name: 'google-site-verification', content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' }]
+  ],
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-148970528-1'
+      }
+    ]
   ]
 }
 pp
