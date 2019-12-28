bf7fd9c431fad36357045f3018236c2b7f14c351
35d <xxx35d@gmail.com>
2019-12-29 00:24:29 +0900

    ✨ fix google site verification tag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,7 @@
 module.exports = {
   title: '35d',
   description: '@___35d のブログサイトです',
+  'google-site-verification': { content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' },
   ga: 'UA-148970528-1',
-  head: [
-    ['google-site-verification', { content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' }]
-  ]
 }
+pp
