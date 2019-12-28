6783148c59d8e4c4497b69d0dee2fef62a0d9d6d
35d <xxx35d@gmail.com>
2019-12-29 01:36:43 +0900

    ✨ add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: '35d',
   head: [
     ['meta', { name: 'google-site-verification', content: 'cGibt4uXK95r4DSrslEC69ZkZopxKnwvaJqSsnFYUew' }]
+    ['link', { rel: 'icon', href: '/favicon.png' }],
   ],
   plugins: [
     [
