e50c87120e7743cef992e5256c3600d8c2465d6c
35d <xxx35d@gmail.com>
2019-12-29 23:10:57 +0900

    ♻️ VuePress eject

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  title: '35d',
-  description: '@35dのブログです。',
+  title: "35d",
+  description: "@35dのブログです。",
   head: [
-    ['meta', { title: '35d' }],
-    ['meta', { property: 'twitter:card', content: 'summary' }],
-    ['link', { rel: 'icon', href: '/favicon.png' }],
+    ["meta", { title: "35d" }],
+    ["meta", { property: "twitter:card", content: "summary" }],
+    ["link", { rel: "icon", href: "/favicon.png" }]
   ],
   plugins: [
     [
-      '@vuepress/google-analytics',
+      "@vuepress/google-analytics",
+      "@vuepress/blog",
       {
-        'ga': 'UA-148970528-1'
+        ga: "UA-148970528-1"
       }
     ]
   ]
-}
+};
