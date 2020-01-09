3f56358b053a4a61f18a1833142192235440f958
35d <xxx35d@gmail.com>
2020-01-09 09:38:26 +0900

    ♻️ fix sort function

--- docs/.vuepress/theme/global-components/PostList.vue
+++ docs/.vuepress/theme/global-components/PostList.vue
@@ -19,11 +19,12 @@ export default {
       return this.$site.pages
         .filter(post => post.path.startsWith("/blog/"))
         .filter(post => !!post.title)
-        .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
         .map(post => {
           post.frontmatter.date = moment(post.frontmatter.date).format("YYYY/MM/DD");
           return post;
-        });
+        })
+        .sort(() => -1);
+      // .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
     },
     categories() {
       return [];
